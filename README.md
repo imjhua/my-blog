b2795b5e9caf355bd27a822cc799f24d1c9e7f1e
bella.v <bella.v@kakaomobility.com>
2022-09-07 19:09:04 +0900

    add

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
   Lia BLOG
 </h1>
 
+- 
 ## ✨ Features
 
 - ...
 - https://jng-u.github.io/gatsby-%EB%B8%94%EB%A1%9C%EA%B7%B8-latex%EB%AC%B8%EB%B2%95-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0
 - https://github.com/DSchau/gatsby-remark-toc
+- https://www.gatsbyjs.com/plugins/@lekoarts/gatsby-theme-minimal-blog/
--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,6 +1,51 @@
 import { css } from '@nfront/global-styles';
 
 const globalStyles = css`
+  @media (max-width: 720px) {  
+    pre{
+      display: none !important;
+    }
+  }
+  
+  html{
+    font-size: 14px !important;
+  }
+  header{
+    margin-bottom: 1rem !important;
+  }
+  main > section{
+    margin: 2rem 0 !important;
+  }
+  main > p{
+    display: none !important;
+  }
+  code{
+    font-family: system-ui !important;
+  }
+  .gatsby-highlight pre:before {
+    font-family: system-ui !important;
+  }
+
+  ul{
+    padding-left: 24px !important;
+    overflow: scroll !important;
+  }
+  h1, h2, h3{
+    margin-top: 1rem;
+  }
+  h1{
+    font-size: 1.5rem !important;
+  }
+  h2{
+    font-size: 1.2rem !important;
+  }
+  h3{
+    font-size: 1.0rem !important;
+  }
+  h4{
+    font-size: 1.0rem !important;
+  }
+
   footer > div:last-child{
     display: none;
   }
