e5f2da114e15a9dee41783d07678a45b8579d449
bella.v <bella.v@kakaomobility.com>
2022-09-08 00:00:15 +0900

    add

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `@lekoarts/gatsby-theme-Lia-blog`,
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
       // See the theme's README for all available options
       options: {
         navigation: [
